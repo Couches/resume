3e020470e7e0d4af21385fa3de9b498e163b7e3e
Robert De Lappe <robertgdel@gmail.com>
2022-11-02 01:06:24 -0700

    Change of color, add text

--- javascript.js
+++ javascript.js
@@ -2,13 +2,16 @@ function openCard(cardID)
 {
     var tabs = document.getElementsByClassName("tab");
     var cards = document.getElementsByClassName("card");
+    var cardText = document.getElementsByClassName("card-body");
 
     for (let i = 0; i < cards.length; i++)
     {
         tabs[i].classList.remove("tab-selected");
         cards[i].classList.remove("card-selected");
+        cardText[i].classList.remove("card-selected");
     }
 
     tabs[cardID].classList.add("tab-selected");
     cards[cardID].classList.add("card-selected");
+    cardText[cardID].classList.add("card-selected");
 }
