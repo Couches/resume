8b93dd8c914945fc5a429cb3c6cfdaa0b32cc17d
Robert De Lappe <robertgdel@gmail.com>
2022-11-24 21:27:02 -0800

    Rework entire project

--- javascript.js
+++ javascript.js
@@ -1,17 +1,8 @@
-function openCard(cardID)
+function toggleDropdown(dropID)
 {
-    var tabs = document.getElementsByClassName("tab");
-    var cards = document.getElementsByClassName("card");
-    var cardText = document.getElementsByClassName("card-body");
-
-    for (let i = 0; i < cards.length; i++)
-    {
-        tabs[i].classList.remove("tab-selected");
-        cards[i].classList.remove("card-selected");
-        cardText[i].classList.remove("card-selected");
-    }
-
-    tabs[cardID].classList.add("tab-selected");
-    cards[cardID].classList.add("card-selected");
-    cardText[cardID].classList.add("card-selected");
+    var drops = document.getElementsByClassName("drop");
+    var drop = drops[dropID];
+    
+    if (drop.classList.contains("expand")) drop.classList.remove("expand");
+    else drop.classList.add("expand");
 }
